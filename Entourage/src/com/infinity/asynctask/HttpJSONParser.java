3409d8d961e885f943a1f3027a8ce57eab0d12ea
damion6891 <damion6891@yahoo.com>
2014-10-12 07:10:23 -0400

    Deleted unused objects

--- Entourage/src/com/infinity/asynctask/HttpJSONParser.java
+++ Entourage/src/com/infinity/asynctask/HttpJSONParser.java
@@ -4,19 +4,11 @@ import org.json.JSONObject;
 
 import com.google.gson.Gson;
 
-public class HttpJSONParser extends HttpParser{
-	
-	
-	public Object parse(JSONObject json, Class<?> myclass){
-		
-		 Gson gson = new Gson(); // Or use new GsonBuilder().create();
-		// MyType target = new MyType();
-		 //String json = gson.toJson(target); // serializes target to Json
-		 Object classObject = gson.fromJson(json.toString(), myclass); // deserializes json into target2
-		
-		
-		return classObject;
-		
-	}
+public class HttpJSONParser extends HttpParser {
 
+	public Object parse(JSONObject json, Class<?> myclass) {
+		Gson gson = new Gson();
+		Object classObject = gson.fromJson(json.toString(), myclass);
+		return classObject;
+	}
 }
