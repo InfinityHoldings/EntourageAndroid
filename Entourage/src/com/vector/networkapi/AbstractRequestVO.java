124784f5c4bf66125ae91fd6e960dc15aed52110
KeonW <keontwilliams@gmail.com>
2015-04-05 19:13:47 -0400

    NetworkAPI; slight changes. committing for merge

--- Entourage/src/com/vector/networkapi/AbstractRequestVO.java
+++ Entourage/src/com/vector/networkapi/AbstractRequestVO.java
@@ -1,5 +1,17 @@
 package com.vector.networkapi;
 
-public class AbstractRequestVO {
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+
+public abstract class AbstractRequestVO {
+	
+	public static final String TAG = AbstractRequestVO.class.getCanonicalName(); 
+	
+	protected abstract String toJSON(); 
+	
+	public abstract List<NameValuePair> toHeaderList(); 
+	
+	
 
 }
